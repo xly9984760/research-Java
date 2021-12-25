fc06fdf5be259b5620d233d4b5e674ea3629915f
xly <9984760@qq.com>
2021-12-25 11:11:00 +0800

    学习左神算法

---
+++ algorithm/src/main/cn/xlystar/Main.java
@@ -0,0 +1,11 @@
+package main.cn.xlystar;
+
+/**
+ * @ClassName: Main
+ * @Author: 99847
+ * @Description:
+ * @Date: 2021/12/25 10:17
+ * @Version: 1.0
+ */
+public class Main {
+}
---
+++ algorithm/src/resource/基础入门班第一课代码/Code06_BSAwesome.java
@@ -0,0 +1,10 @@
+package resource.基础入门班第一课代码;
+
+public class Code06_BSAwesome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
